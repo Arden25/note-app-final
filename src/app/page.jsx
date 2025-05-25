e0371cc1775b9916f2034c5d3db87bfb592a90f1
Arden25 <ariadennya2503@gmail.com>
2025-05-25 13:36:53 +0700

    menambah home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { useEffect, useState } from "react";
 import jwt from "jsonwebtoken";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -28,18 +30,6 @@ export default function Home() {
         <Button variant="outline" size="lg" className="text-lg border-blue-200 text-blue-800 hover:text-blue-800 hover:bg-blue-50">
           Buat Catatan Baru
         </Button>
-      </div>
-      <div className="mt-16">
-        <h2 className="text-2xl font-bold text-gray-800 mb-4">Features</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl mx-auto">
-          <Card
-            title="Create Notes"
-            content="Create and organize your notes with ease. Add title and content to
-              each note."
-          />
-          <Card title={"View Notes"} content={"Browse all your notes and find them easily whenever you need."} />
-          <Card title={"Edit Notes"} content={"Update and modify your notes whenever needed with just a few clicks."} />
-        </div>
       </div>
     </div>
   );
